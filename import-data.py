c0bd0f09bb2924c71404f34c1ec56dda7fb25fe1
shreyasravi <ravi.s@ei.com>
2014-11-24 17:29:04 -0500

    Added many more models and fields.
    Adjusted the import file to record everything.

--- import-data.py
+++ import-data.py
@@ -1,11 +1,183 @@
+#import sys
+#sys.path.append('C:\\Personal\\nbastats\\nbastats\\nbastats\\settings.py')
+#sys.path.append('C:\\Personal\\nbastats\\nbastats\\nbastats')
+#sys.path.append('C:\\Personal\\nbastats\\nbastats')
+#print(sys.path)
+
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nbastats.settings'
 import csv, datetime
 from players import models
 import django
-
-
-
-
 django.setup()
+
+
+
+def import_play(row, home_team, away_team, game):
+		""""Imports one line of play-by-play csv data"""
+		
+		a1, created = models.Player.objects.get_or_create(first_name=row['a1'].split()[0].lower(),
+										  last_name=row['a1'].split()[1].lower(),
+										  team=away_team)
+		a2, created = models.Player.objects.get_or_create(first_name=row['a2'].split()[0].lower(),
+										  last_name=row['a2'].split()[1].lower(),
+										  team=away_team)
+		a3, created = models.Player.objects.get_or_create(first_name=row['a3'].split()[0].lower(),
+										  last_name=row['a3'].split()[1].lower(),
+										  team=away_team)
+		a4, created = models.Player.objects.get_or_create(first_name=row['a4'].split()[0].lower(),
+										  last_name=row['a4'].split()[1].lower(),
+										  team=away_team)
+		a5, created = models.Player.objects.get_or_create(first_name=row['a5'].split()[0].lower(),
+										  last_name=row['a5'].split()[1].lower(),
+										  team=away_team) 
+		h1, created = models.Player.objects.get_or_create(first_name=row['h1'].split()[0].lower(),
+										  last_name=row['h1'].split()[1].lower(),
+										  team=home_team)
+		h2, created = models.Player.objects.get_or_create(first_name=row['h2'].split()[0].lower(),
+										  last_name=row['h2'].split()[1].lower(),
+										  team=home_team)
+		h3, created = models.Player.objects.get_or_create(first_name=row['h3'].split()[0].lower(),
+										  last_name=row['h3'].split()[1].lower(),
+										  team=home_team)
+		h4, created = models.Player.objects.get_or_create(first_name=row['h4'].split()[0].lower(),
+										  last_name=row['h4'].split()[1].lower(),
+										  team=home_team)
+		h5, created = models.Player.objects.get_or_create(first_name=row['h5'].split()[0].lower(),
+										  last_name=row['h5'].split()[1].lower(),
+										  team=home_team)
+		period=row['period']
+		minutes, seconds = int(row['time'].split(':')[0]), int(row['time'].split(':')[1])
+		time = datetime.timedelta(minutes=minutes, seconds=seconds)
+		ejected = None
+		if row['etype'] == 'ejected':
+			ejected = models.Player.objects.get(first_name=row['player'].split()[0].lower(),
+										  last_name=row['player'].split()[1].lower())
+		number = None
+		if row['num']:
+			number = row['num']
+		etype = row['etype']
+		player = None
+		if row['player']:
+			player = models.Player.objects.get(
+									first_name=row['player'].split()[0].lower(),
+									last_name=row['player'].split()[1].lower())
+		play, created = models.Play.objects.get_or_create(game=game, time=time,
+														  period=period, 
+														  ejected=ejected,
+														  number=number,
+														  etype=etype,
+														  player=player)
+		play.home_5.add(h1, h2, h3, h4, h5)
+		play.away_5.add(a1, a2, a3, a4, a5)
+		play.save()
+		print(play.pk, play.period, play.home_5.all())
+		# Handling a shot instance
+		if row['etype'] == 'shot' or row['etype'] == 'free throw':
+			shot_type = {'free throw':'FT', 'jump':'J', 
+						 'driving layup':'DL', '3pt':'3pt shot', 
+					     'pullup jump':'PJ', 'layup':'L',
+						 'dunk':'D', 'shot':'S', 'running':'R', 'tip':'T'}
+			if row['etype'] == 'free throw':
+				type = shot_type[row['etype']]
+			else:
+				type = shot_type[row['type']]
+			result = row['result']
+			x, y = None, None
+			if row['x']:
+				x, y = row['x'], row['y']
+			shooter = models.Player.objects.get(
+										  first_name=row['player'].split()[0].lower(),
+										  last_name=row['player'].split()[1].lower())
+			assist = None
+			if row['assist']:
+				assist = models.Player.objects.get(
+										  first_name=row['assist'].split()[0].lower(),
+										  last_name=row['assist'].split()[1].lower())
+			block = None
+			if row['block']:
+				block = models.Player.objects.get(
+										  first_name=row['block'].split()[0].lower(),
+										  last_name=row['block'].split()[1].lower())
+			points = 0
+			if row['points']:
+				points = row['points']
+			else: 
+				if 'free throw' == row['etype'] and 'made' == row['result']:
+					points = 1
+			shot, created = models.Shot.objects.get_or_create(result=result,
+												type=type, x=x, y=y, 
+												shooter=shooter, assist=assist,
+												block=block, team=shooter.team,
+												play=play, points=points)
+												
+			shot.save()
+		# Handling a foul instance
+		if row['etype'] == 'foul':
+			foul_type = {'personal':'P', 'shooting':'S', 'offensive':'O',
+						  'loose ball':'LB', 'flagrant':'F'}
+			type = foul_type[row['type']]
+			fouler = models.Player.objects.get(
+										  first_name=row['player'].split()[0].lower(),
+										  last_name=row['player'].split()[1].lower())
+			fouled = models.Player.objects.get(
+										  first_name=row['opponent'].split()[0].lower(),
+										  last_name=row['opponent'].split()[1].lower())
+			foul, created = models.Foul.objects.get_or_create(type=type,
+										  fouler=fouler, fouled=fouled,
+										  play=play, team=fouler.team)
+			foul.save()
+		# Handling a turnover instance
+		if row['etype'] == 'turnover':
+			turnover_type = {'bad pass':'BP', 'lost ball':'LB', 
+						'kickball violation':'KV',
+						'steps out of bounds':'OB', 
+						'traveling':'T', 'lost ball':'LB'}
+			type = None
+			if row['reason']:
+				type = turnover_type[row['reason']]
+			committed = None
+			if row['player']:
+				committed = models.Player.objects.get(
+										  first_name=row['player'].split()[0].lower(),
+										  last_name=row['player'].split()[1].lower())
+			steal = None
+			if row['steal']:
+				steal = models.Player.objects.get(
+										  first_name=row['steal'].split()[0].lower(),
+										  last_name=row['steal'].split()[1].lower())
+			team = models.Team.objects.get(name=row['team'])
+			turnover, created = models.Turnover.objects.get_or_create(type=type,
+										  committed=committed, steal=steal,
+										  play=play, team=team)
+			turnover.save()
+		if row['etype'] == 'rebound':
+			type = row['type']
+			player = None
+			if row['player']:
+				player = models.Player.objects.get(
+										  first_name=row['player'].split()[0].lower(),
+										  last_name=row['player'].split()[1].lower())
+			team = models.Team.objects.get(name=row['team'])
+			rebound, created = models.Rebound.objects.get_or_create(type=type,
+										  player=player, team=team, play=play)
+			rebound.save()
+		if row['etype'] == 'jump ball':
+			home = models.Player.objects.get(
+										  first_name=row['home'].split()[0].lower(),
+										  last_name=row['home'].split()[1].lower())
+			away = models.Player.objects.get(
+										  first_name=row['away'].split()[0].lower(),
+										  last_name=row['away'].split()[1].lower())
+			possession = models.Player.objects.get(
+										  first_name=row['possession'].split()[0].lower(),
+										  last_name=row['possession'].split()[1].lower())
+			jumpball, created = models.Jumpball.objects.get_or_create(home=home,
+														away=away, play=play,
+														possession=possession)
+
+
 
 def import_game(filename):
 	year, month, day = int(filename.split('_')[4].split('.')[0]),int(filename.split('_')[2]),\
@@ -20,63 +192,7 @@ def import_game(filename):
 										
 	input_file = csv.DictReader(open('C:\\Personal\\nbastats\\nbastats\\players\\data\\games_play-by-play\\'+filename))
 	for row in input_file:
-		a1, created = models.Player.objects.get_or_create(first_name=row['a1'].split()[0],
-										  last_name=row['a1'].split()[1],
-										  team=away_team)
-		a2, created = models.Player.objects.get_or_create(first_name=row['a2'].split()[0],
-										  last_name=row['a2'].split()[1],
-										  team=away_team)
-		a3, created = models.Player.objects.get_or_create(first_name=row['a3'].split()[0],
-										  last_name=row['a3'].split()[1],
-										  team=away_team)
-		a4, created = models.Player.objects.get_or_create(first_name=row['a4'].split()[0],
-										  last_name=row['a4'].split()[1],
-										  team=away_team)
-		a5, created = models.Player.objects.get_or_create(first_name=row['a5'].split()[0],
-										  last_name=row['a5'].split()[1],
-										  team=away_team) 
-		h1, created = models.Player.objects.get_or_create(first_name=row['h1'].split()[0],
-										  last_name=row['h1'].split()[1],
-										  team=home_team)
-		h2, created = models.Player.objects.get_or_create(first_name=row['h2'].split()[0],
-										  last_name=row['h2'].split()[1],
-										  team=home_team)
-		h3, created = models.Player.objects.get_or_create(first_name=row['h3'].split()[0],
-										  last_name=row['h3'].split()[1],
-										  team=home_team)
-		h4, created = models.Player.objects.get_or_create(first_name=row['h4'].split()[0],
-										  last_name=row['h4'].split()[1],
-										  team=home_team)
-		h5, created = models.Player.objects.get_or_create(first_name=row['h5'].split()[0],
-										  last_name=row['h5'].split()[1],
-										  team=home_team)
-		if row['team'] == away_team.name:
-			team = away_team
-		else: 
-			team = home_team
-		period=row['period']
-		if row['points']:
-			points = row['points']
-		else: 
-			if 'free throw' == row['etype'] and 'made' == row['result']:
-				points = 1
-			else:
-				points = 0
-		minutes, seconds = int(row['time'].split(':')[0]), int(row['time'].split(':')[1])
-		if row['num']:
-			number=row['num']
-		else:
-			number = None
-		play, created = models.Play.objects.get_or_create(game=game, team=team,
-												   period=period, points=points,
-												   time=datetime.timedelta(
-																minutes=minutes, 
-																seconds=seconds),
-												   number=number)
-		play.home_5.add(h1, h2, h3, h4, h5)
-		play.away_5.add(a1, a2, a3, a4, a5)
-		play.save()
-												   
+		import_play(row, home_team, away_team, game)
 
 filename = 'BRO_CHA_3_26_2014.csv'
 import_game(filename)
--- players/models.py
+++ players/models.py
@@ -8,42 +8,68 @@ class Play(models.Model):
 	time = timedelta.fields.TimedeltaField()
 	home_5 = models.ManyToManyField('Player', related_name='home_5')
 	away_5 = models.ManyToManyField('Player', related_name='away_5')
-	points = models.PositiveSmallIntegerField()
-	team = models.ForeignKey('Team')
-	number = models.PositiveSmallIntegerField(null=True) # For free throws 
-	shot = models.ForeignKey('Shot', null=True) #There may not be a shot on the play
+	ejected = models.ForeignKey('Player', related_name='ejected', null=True)
+	# For free throws: ensures that we get a unique play for each line of the csv
+	number = models.PositiveSmallIntegerField(null=True)
+	etype = models.CharField(max_length=30) # Ensure we get unique plays
+	player = models.ForeignKey('Player', related_name='player', null=True)
 
 class Shot(models.Model):
 	
-	SHOT_CHOICES = (('FT', 'Free Throw'),('J', 'Jump Shot'), ('DL', 'Driving Layup')
-					('3p', '3pt Shot'), ('PJ', 'Pullup Jumper'), ('L', 'Layup')
-					('D', 'Dunk'), ('S', 'Shot'))
+	SHOT_CHOICES = (('FT', 'Free Throw'),('J', 'Jump Shot'), ('DL', 'Driving Layup'),
+					('3p', '3pt Shot'), ('PJ', 'Pullup Jumper'), ('L', 'Layup'),
+					('D', 'Dunk'), ('S', 'Shot'), ('R', 'Running'), ('T', 'Tip'))
 	result = models.BooleanField() # True = made, False = missed
-	type = models.CharField(max_length = 2, choices = SHOT_CHOICES)
-	x = models.PositiveSmallIntegerField()
-	y = models.PositiveSmallIntegerField()
+	type = models.CharField(max_length=2, choices=SHOT_CHOICES)
+	x = models.PositiveSmallIntegerField(null=True) # null=True for free throws
+	y = models.PositiveSmallIntegerField(null=True)
 	shooter = models.ForeignKey('Player', related_name='shooter')
 	assist = models.ForeignKey('Player', related_name='assist', null=True)
 	block = models.ForeignKey('Player', related_name='block', null=True)
+	play = models.OneToOneField('Play')
+	points = models.PositiveSmallIntegerField()
+	team = models.ForeignKey('Team')
 
 class Foul(models.Model):
+
 	FOUL_CHOICES = (('P', 'Personal'), ('S', 'Shooting'), ('O', 'Offensive'),
 					('LB', 'Loose Ball'), ('F', 'Flagrant'))
 					
 	fouler = models.ForeignKey('Player', related_name='fouler')
 	fouled = models.ForeignKey('Player', related_name='fouled')
+	play = models.OneToOneField('Play')
+	team = models.ForeignKey('Team')
+	type = models.CharField(max_length=2, choices=FOUL_CHOICES)
 	
 class Turnover(models.Model):
-	TURNOVER_CHOICES = (('BP', 'Bad Pass', ('LB', 'Lost Ball'), 
+	
+	TURNOVER_CHOICES = (('BP', 'Bad Pass'), ('LB', 'Lost Ball'), 
 						('KV', 'Kickball Violation'),
 						('OB', 'Steps Out of Bounds'), 
 						('T', 'Travelling'), ('LB', 'Lost Ball'))
 					
-	fouler = models.ForeignKey('Player', related_name='fouler')
-	fouled = models.ForeignKey('Player', related_name='fouled')
+	# Occassionally, a turnover is not charged to a player, hence null=True
+	committed = models.ForeignKey('Player', related_name='committed', null=True)
+	steal = models.ForeignKey('Player', related_name='steal', null=True)
+	type = models.CharField(max_length=2, choices=TURNOVER_CHOICES, null=True)
+	team = models.ForeignKey('Team')
+	play = models.OneToOneField('Play')
+
+class Rebound(models.Model):
 	
+	REBOUND_CHOICES = (('def', 'Defensive'), ('off', 'Offensive'))
+	player = models.ForeignKey('Player', null=True)
+	type = models.CharField(max_length=3, choices=REBOUND_CHOICES)
+	team = models.ForeignKey('Team')
+	play = models.OneToOneField('Play')
 
+class Jumpball(models.Model):
 
+	home = models.ForeignKey('Player', related_name='home')
+	away = models.ForeignKey('Player', related_name='away')
+	possession = models.ForeignKey('Player', related_name='possession')
+	play = models.OneToOneField('Play')
+	
 class Game(models.Model): 
 	
 	home_team = models.ForeignKey('Team', related_name = 'home_team')
